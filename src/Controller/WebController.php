cc1ca266c2a9875d6f499d22b801fe75ba06ed08
Liligeia <melanie.xelm@gmail.com>
2020-11-04 12:10:40 +0100

    update composer + form contact

--- src/Controller/WebController.php
+++ src/Controller/WebController.php
@@ -6,6 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class WebController extends AbstractController{
 
@@ -32,9 +38,42 @@ class WebController extends AbstractController{
 /**
 * @Route("/contact", name="contact")
 */
-	public function contact_index() {
-		return $this->render('website/contact/index.html.twig');
-	}	
+	public function contact_index(Request $request)
+	{
+		$form = $this->createFormBuilder()
+			->add('nom', TextType::class)
+			->add('email', EmailType::class)
+			->add('objet', TextType::class)
+			->add('message', TextareaType::class)
+			->add('envoyer', SubmitType::class)
+			->getForm()
+		;
+
+			$form->handleRequest($request);
+
+			if ($form->isSubmitted() && $form->isValid()) {
+
+				$data = $form->getData();
+
+				dump($data);
+
+				$message = (new \Swift_Message())
+					->setSubject('Support Form Submission')
+					->setFrom($data['email'])
+					->setTo('melanie.xelm@gmail.com')
+					->setBody(
+						$form->getData()['message'],
+						'text/plain'
+					);
+
+					$this->get('mailer')->send($message);
+			}
+
+
+		return $this->render('website/contact/index.html.twig', [
+			'contact_form' => $form->createView()
+		]);
+	}
 /**
 * @Route("/photo", name="photo")
 */
